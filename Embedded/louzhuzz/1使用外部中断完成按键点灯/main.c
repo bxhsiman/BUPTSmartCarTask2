e6e5669898cf2d208b8868225398f47f2cd14f8d
louzhuzz <liuyuyangxxx@163.com>
2023-09-21 20:56:27 +0800

    [add]修改外部中断

--- Embedded/louzhuzz/1使用外部中断完成按键点灯/main.c
+++ Embedded/louzhuzz/1使用外部中断完成按键点灯/main.c
@@ -1,12 +1,31 @@
 #include <STC89C5xRC.H>
 
+sbit k3=P3^2; 
+sbit led=P2^0; 
+
+void delay(unsigned int i)
+{
+	while(i--);
+}
+
+void Int0Init()
+{
+	IT0=1; // 
+	EX0=1;
+	EA=1;
+}
+
 void main()
 {
-	while(1)
+	Int0Init();
+	while(1);
+}
+
+void Int0() interrupt 0
+{
+	delay(1000);
+	if(k3==0)
 	{
-		if(P31 == 0)
-			P20 = 0;
-		else
-			P20 = 1;
+		led = ~led;
 	}
 }
