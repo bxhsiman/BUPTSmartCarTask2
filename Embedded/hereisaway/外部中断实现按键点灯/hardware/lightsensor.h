9ba513bce10ff9fb511c6af56944a18b31bcb5ee
Yuki Nagato <3335126979@qq.com>
2023-10-06 21:24:03 +0800

    add a folder in embedded

---
+++ Embedded/hereisaway/外部中断实现按键点灯/hardware/lightsensor.h
@@ -0,0 +1,5 @@
+#ifndef __LIGHTSENSOR_h
+#define __LIGHTSENSOR_h
+uint8_t lightsensor_get(void);
+void lightsensor_init(void);
+#endif
---
+++ Embedded/hereisaway/外部中断实现按键点灯/key/key.h
@@ -0,0 +1,5 @@
+#ifndef __KEY_H
+#define __KEY_H
+	void key_init(void);
+	uint8_t get_keynum(void);
+#endif
