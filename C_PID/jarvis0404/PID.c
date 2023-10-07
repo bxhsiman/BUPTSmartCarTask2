c84f12e3067c386ff13f2f74dddba6563b5ea0e4
Eren <347561039@qq.com>
2023-10-07 20:28:58 +0800

    version 1.0

--- C_PID/jarvis0404/PID.c
+++ C_PID/jarvis0404/PID.c
@@ -6,10 +6,10 @@ typedef struct
     float set;
     float actual;
     float err;
-    float last_err;
-    float kp, ki, kd;
-    float voltage;
+    float last_err, last_last_err;
     float integral;
+    float kp, ki, kd;
+    float PIDOutput;
 } PID;
 
 void PID_Init(PID *PIDStruct)
@@ -18,16 +18,17 @@ void PID_Init(PID *PIDStruct)
     PIDStruct->set = 0.0;
     PIDStruct->err = 0.0;
     PIDStruct->last_err = 0.0;
+    PIDStruct->last_last_err = 0.0;
     PIDStruct->kp = 0.0;
     PIDStruct->ki = 0.0;
     PIDStruct->kd = 0.0;
-    PIDStruct->voltage = 0.0;
+    PIDStruct->PIDOutput = 0.0;
     PIDStruct->integral = 0.0;
 }
 
 void PID_SetParameter(PID *PIDStruct, float kp, float ki, float kd); // 设置PID参数
 float PID_PostionalPID(PID *PIDStruct, float speed);
-int PID_IncrementalPID(PID *PIDStruct); // 增量式PID实现
+float PID_IncrementalPID(PID *PIDStruct, float speed); // 增量式PID实现
 
 int main(void)
 {
@@ -38,21 +39,9 @@ int main(void)
     uint16_t cnt = 1000;
     while (cnt--)
     {
-        printf("PID: %f\n", PID_PostionalPID(&pid, 100.0));
+        printf("PID: %f\n", PID_IncrementalPID(&pid, 100.0));
     }
     return 0;
-}
-
-float PID_PostionalPID(PID *PIDStruct, float speed) // 位置式PID实现
-{
-    PIDStruct->set = speed;
-    PIDStruct->err = PIDStruct->set - PIDStruct->actual;
-    PIDStruct->integral += PIDStruct->err;
-    PIDStruct->voltage = (PIDStruct->kp * PIDStruct->err) + (PIDStruct->ki * PIDStruct->integral) + (PIDStruct->kd * (PIDStruct->err - PIDStruct->last_err));
-    PIDStruct->last_err = PIDStruct->err;
-    PIDStruct->actual = PIDStruct->voltage * 1.0;
-
-    return PIDStruct->actual;
 }
 
 void PID_SetParameter(PID *PIDStruct, float kp, float ki, float kd)
@@ -60,4 +49,30 @@ void PID_SetParameter(PID *PIDStruct, float kp, float ki, float kd)
     PIDStruct->kp = kp;
     PIDStruct->ki = ki;
     PIDStruct->kd = kd;
+}
+
+float PID_PostionalPID(PID *PIDStruct, float speed) // 位置式PID实现
+{
+    PIDStruct->set = speed;
+    PIDStruct->err = PIDStruct->set - PIDStruct->actual;
+    PIDStruct->integral += PIDStruct->err;
+    PIDStruct->PIDOutput = (PIDStruct->kp * PIDStruct->err) + (PIDStruct->ki * PIDStruct->integral) + (PIDStruct->kd * (PIDStruct->err - PIDStruct->last_err));
+    PIDStruct->last_err = PIDStruct->err;
+    PIDStruct->actual = PIDStruct->PIDOutput * 1.0;
+
+    return PIDStruct->actual;
+}
+
+float PID_IncrementalPID(PID *PIDStruct, float set)
+{
+    PIDStruct->set = set;
+    PIDStruct->err = PIDStruct->set - PIDStruct->actual;
+    float increment = PIDStruct->kp * (PIDStruct->err - PIDStruct->last_err) + PIDStruct->ki * PIDStruct->err + PIDStruct->kd * (PIDStruct->err - 2 * PIDStruct->last_err + PIDStruct->last_last_err);
+    PIDStruct->last_last_err = PIDStruct->last_err;
+    PIDStruct->last_err = PIDStruct->err;
+    PIDStruct->actual += increment;
+
+    PIDStruct->PIDOutput = PIDStruct->actual * 1.0;
+
+    return PIDStruct->actual;
 }
